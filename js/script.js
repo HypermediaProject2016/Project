3cd758a970ca9e40d00480dc968d1d1c2d77a22c
HypermediaProject2016 <simone.sipala@hotmail.it>
2016-07-18 12:08:19 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(function(){
 	"use strict";
-    $("#incl_header").load("intestazione.html #divmenu");
-    $("#incl_footer").load("footer.html #footer");
+    $('#incl_header').load('intestazione.html #divmenu');
+    $('#incl_footer').load('footer.html #footer');
 });
